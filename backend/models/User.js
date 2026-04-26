7574f25e1382c05462dd6d941765be80be509160
drv-01 <officialdrvkumar@gmail.com>
2026-04-26 18:28:25 +0530

    fix: prevent mongoose model re-registration error on serverless

--- backend/models/User.js
+++ backend/models/User.js
@@ -41,4 +41,5 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
     return await bcrypt.compare(enteredPassword, this.password);
 };
 
-export default mongoose.model('User', userSchema);
+const User = mongoose.models.User || mongoose.model('User', userSchema);
+export default User;
